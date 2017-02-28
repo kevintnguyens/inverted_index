defb97c912aa4cb403458bfda9e7f9f13b23df4c
Quinn Casey <qcasey1@gmail.com>
2017-02-27 20:40:18 -0800

    Added documentation

--- lab3.py
+++ lab3.py
@@ -1,10 +1,215 @@
-# Lab 3
+# Lab 3, CS 121
+# Python 2.7
+# Quinn Casey 78016851
+# K dogg
+# D dogg
+
+###
+#
+#   Main Goals of program (creating 3 dicts):
+#
+#   1. Parse bookkeeping files and create dict of docCodes and document URLS
+#      {'folderID:docID' : 'documentURL'}
+#      This is useful for returning search results later
+#
+#   2. Transverse documents in file system, parse with BeautifulSoup.
+#      Return a NEW dictionary of [{'folderID:docID' : {'term' : frequency} }].
+#      This is useful for not having to do 95% of the proj before Wed
+#      Can be depreciated later if memory is an issue
+#
+#   3. (Should be step 2.5) Transform dict from step 2 into inverted index.
+#      Structured like {'term' : ['folderID:docID']} (tentative - will need tag support) 
+#      This is useful as a small size, sorted dict by TF-IDF.
+#      Essentially compressed by using docCodes ['folderID:docID']
+#
+#   4. Add a search functionality? Definitely need, not sure how to implement
+#      She'll probably clarify next week
+#      
+###
+
+###
+# TODO BY WEDNESDAY March 1st:
+#   Steps 1 & 2 above. Functions:
+#   - getJson
+#   - getTsv
+#   - parseDocumentDict
+#   - printDocumentDict for our M1 report
+###
+
+###
+# TODO BY March 8th:
+#   Step 3 above. Functions:
+#   - createInvertedIndex
+#   - searchIndex
+###
+
+###
+# TODO by final due date
+#   - IDK
+###
 
 import json
 import ijson
 
-with open('bookkeeping.json', 'r') as fd:
-    parser = ijson.parse(fd)
+def getJson(jsonFile):
+    """
+    *** Reads a json file
+    *** NOTE: READ USING BLOCKS OR WHATEVER. NOT ALL AT ONCE (ijson?)
+    *** Returns a dict {'folderID:docID' : 'documentURL'}
+    """
 
-    for item in ijson.items():
-        print(item)
+    
+    '''
+    with open(jsonFile, 'r') as fd:
+        parser = ijson.parse(fd)
+
+        for item in ijson.items():
+            print(item)
+        '''
+
+
+    return dict()
+
+def getTsv(tsvFile):
+    """
+    *** Reads a tsv file, splits by newlines & spaces?
+    *** NOTE: READ USING BLOCKS OR WHATEVER. NOT ALL AT ONCE
+    *** Returns a dict {'folderID:docID' : 'documentURL'}
+    """
+
+    return dict()
+
+
+def parseDocumentDict(documentDict):
+    """
+    *** Meaty part of the script. Used for M1
+    *** For each document in documentDict, split the key docCode ('folderID:docID')
+    *** by ':' -- get document in [0] (folderID) with name [1] (docID)
+    *** Open document and parse with BS.
+    *** FOR NOW: STRIP TAGS AND GET WORDS (TEXT) ONLY!
+    *** Add terms and their respective freq to a dict. Append dict of
+    *** 'folderID:docID' to final index.
+    *** Returns {'folderID:docID' : {'term' : frequency} }
+
+    *** TODO LATER (NOT WED):
+    *** - Tag support.
+    ***   Differentiate between <p>What's gucci</p> and <h1>What's gucci</h1>
+    ***   maybe in createInvertedIndex() idk
+    """
+
+    index = dict()
+
+    ''' #mostly pseudo code
+    for docCode in documentDict:
+        docCodeParse = docCode.split(':')
+        BS.open(docCodeParse[0]+'/'+docCodeParse[1])
+        BS.stripTags()
+        
+        pureText = BS.getText()
+        termFreqDict = dict()
+        
+        for term in pureText.split():
+            termFreqDict[term] += 1
+
+        # append to final index
+        index[docCode] = termFreqDict
+    
+    '''
+
+    return index
+
+def printDocumentDict(indexDict):
+    """
+    *** FOR WEDNESDAY'S M1 REPORT ONLY!!
+    *** Print the following info (to file maybe):
+    ***     - # of documents in index
+    ***     - # of unique words
+    ***     - computed size of index file
+    *** Returns void
+    """
+
+    # num of documents
+    #print(len(indexDict))
+
+    # num of unique words
+    '''
+    termSet = set()
+    # iterate over terms, add to set
+    for document in indexDict:
+        for term in indexDict[document]:
+            termSet.add(term)
+    print(len(termSet))
+    '''
+
+    # Compute size of index file
+    
+
+def createInvertedIndex(indexDict):
+    """
+    *** Given an index [{'folderID:docID' : {'term' : frequency} }]
+    *** 1. Create new index, in the structure of {'term' : ['folderID:docID']}
+    *** EDIT STRUCTURE MAYBE? May need to change ['folderID:docID'] to {'folderID:docID' : frequency}
+    *** Returns {'term' : ['folderID:docID']}
+    """
+
+# POSSIBLY NOT NEEDED??
+def searchIndexUI():
+    """
+    *** Prompts user for valid input??? NECESSARY?
+    *** Returns a search query string
+    """
+
+def searchIndex(inIndex, query):
+    """
+    *** Given an Inverted Index and query
+    *** 1. Compute TF-IDF of documents relative to query
+    *** 2. Sort by highest ranking folderID:docID (docCode)
+    *** 3. Return an ordered list of ['folderID:docID'] as a search result
+    """
+
+if __name__ == "__main__":
+
+    #    #
+    # M1 #
+    #    #
+
+    # Get data from json (dict)
+    comboDict = getJson('bookkeeping.json')
+
+    # Get data from tsv (dict) & append to json data
+    comboDict.update(getTsv('bookkeeping.tsv'))
+
+    # With the large combined dict, parse through each document
+    # structured as {'folderID:docID' : 'documentURL'}
+    # Return an index (list of dicts of dicts)
+    #   index = [{'folderID:docID' : {'term' : frequency} }]
+    simpleDocIndex = parseDocumentDict(comboDict)
+
+    # FOR WEDNESDAY ONLY, COMMENT OUT IN PRODUCTION
+    printDocumentDict(simpleDocIndex)
+
+    #    #
+    # M2 #
+    #    #
+
+    # Create legit inverted index
+    '''index = createInvertedIndex(simpleDocIndex)'''
+
+    # Search index for given query
+    '''results = searchIndex(index, "crista lopes") # also the other 2 testing queries
+'''
+    
+    # Print query results (all relevant document URLS)
+    '''
+    print("Found "+len(results)+" results!")
+    for docCode in results:
+        print(simpleDocIndex[docCode])
+    '''
+    
+    #    #
+    # M3 #
+    #    #
+    # who da hell knows
+    
+
+    
